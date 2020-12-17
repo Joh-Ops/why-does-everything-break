e9422bddf064065e15e98c0f375caafe6cde79dd
Joh-Ops <52340127+Joh-Ops@users.noreply.github.com>
2020-12-17 20:47:57 +1100

    please fix

--- panos/network.py
+++ panos/network.py
@@ -1,10 +1,11 @@
 import traceback
 from minecraft.exceptions import LoginDisconnect
 from minecraft.networking.connection import Connection
+from minecraft.networking.packets import serverbound
 
 
 def cracked(address, port):
-    connection = Connection(address, port, username='test')
+    connection = Connection(address, port, username='panos')
     try:
         connection.connect()
     except LoginDisconnect or ConnectionRefusedError:
@@ -15,9 +16,16 @@ def cracked(address, port):
         connection.disconnect()
         return True
 
+def test():
+    print(cracked('127.0.0.1', 25565))
+    print('a')
+
 def status(address, port):
     try:
         response = Connection(address, port).status()
+        if not response:
+            return None
+
         status = response
         status['players'] = [player['name'] for player in response['players']['sample']]
         status['max_players'] = response['players']['max']

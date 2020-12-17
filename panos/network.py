cb5b428b339badcc64eb6f93d2e143a31003ffbe
Joh-Ops <52340127+Joh-Ops@users.noreply.github.com>
2020-12-18 09:55:10 +1100

    cracked test finally fixed

--- panos/network.py
+++ panos/network.py
@@ -1,24 +1,41 @@
 import traceback
+from time import sleep
+from queue import Queue
+
 from minecraft.exceptions import LoginDisconnect
+from minecraft.networking.packets import clientbound
 from minecraft.networking.connection import Connection
-from minecraft.networking.packets import serverbound
 
 
-def cracked(address, port):
-    connection = Connection(address, port, username='panos')
-    try:
-        connection.connect()
-    except LoginDisconnect or ConnectionRefusedError:
-        return False
-    except:
-        traceback.print_exc()
+def network_exception_handler(e, info):
+    if isinstance(e, (LoginDisconnect, ConnectionRefusedError, ValueError)):
+        pass
     else:
-        connection.disconnect()
-        return True
+        traceback.print_tb(e)
 
-def test():
-    print(cracked('127.0.0.1', 25565))
-    print('a')
+def is_cracked(address, port):
+    result = Queue()
+    connection = Connection(address, port, username='panos')
+
+    @connection.listener(clientbound.login.DisconnectPacket, early=True)
+    def handle_exit(packet):
+        result.put(False)
+
+    @connection.listener(clientbound.play.JoinGamePacket)
+    def handle_join_game(packet):
+        result.put(True)
+
+    connection.register_exception_handler(network_exception_handler)
+
+    connection.connect()
+    while result.empty():
+        sleep(0.05)
+
+    is_cracked = result.get()
+    if connection.connected:
+        connection.disconnect()
+    return is_cracked
+
 
 def status(address, port):
     try:
